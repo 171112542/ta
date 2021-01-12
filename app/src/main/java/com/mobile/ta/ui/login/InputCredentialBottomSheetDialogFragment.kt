c399cf05e10820c99e41c62c85fde9bfbb030ec2
syntialai <53588149+syntialai@users.noreply.github.com>
2021-01-12 17:47:38 +0700

    Add login feature and bottom sheet to input credentials
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ app/src/main/java/com/mobile/ta/ui/login/InputCredentialBottomSheetDialogFragment.kt
@@ -0,0 +1,35 @@
+package com.mobile.ta.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mobile.ta.databinding.FragmentInputCredentialBottomSheetBinding
+
+class InputCredentialBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    companion object {
+        fun newFragment(onSubmitListener: (String) -> Unit) = InputCredentialBottomSheetDialogFragment().apply {
+            this.onSubmitListener = onSubmitListener
+        }
+    }
+
+    private lateinit var binding: FragmentInputCredentialBottomSheetBinding
+    private lateinit var onSubmitListener: (String) -> Unit
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentInputCredentialBottomSheetBinding.inflate(inflater, container, false)
+        with(binding) {
+            buttonCloseDialog.setOnClickListener {
+                dismiss()
+            }
+            buttonSubmitCredentials.setOnClickListener {
+                onSubmitListener.invoke(editTextInputCredential.text.toString())
+                dismiss()
+            }
+        }
+        return binding.root
+    }
+}
