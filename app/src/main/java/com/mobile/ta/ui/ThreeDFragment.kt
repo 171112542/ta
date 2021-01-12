3e126850b35e0b647c29bc4ba8c3ff55c4fe7c0e
Steven <hikarizlo122@gmail.com>
2021-01-13 01:35:45 +0700

    update threeDFragment ui using sketchfab's sample

--- app/src/main/java/com/mobile/ta/ui/ThreeDFragment.kt
+++ app/src/main/java/com/mobile/ta/ui/ThreeDFragment.kt
@@ -1,6 +1,36 @@
 package com.mobile.ta.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import com.mobile.ta.databinding.FragmentThreeDBinding
 
 class ThreeDFragment : Fragment() {
+    private lateinit var binding : FragmentThreeDBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentThreeDBinding.inflate(inflater, container, false)
+        binding.apply {
+            webview.settings.allowContentAccess = true
+            webview.settings.javaScriptEnabled = true
+            webview.loadUrl("file:///android_asset/sketchfab-ex.html")
+            webview.webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    return true
+                }
+            }
+        }
+        return binding.root
+    }
 }
