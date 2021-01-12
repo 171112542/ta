6a11e07b73433dcec3eaec609d8dc8a9c15ca2eb
syntialai <53588149+syntialai@users.noreply.github.com>
2021-01-12 22:01:05 +0700

    Add registration error validation, dagger hilt for injection, utils
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- app/src/main/java/com/mobile/ta/viewmodel/login/RegistrationViewModel.kt
+++ app/src/main/java/com/mobile/ta/viewmodel/login/RegistrationViewModel.kt
@@ -1,17 +1,31 @@
 package com.mobile.ta.viewmodel.login
 
 import android.graphics.Bitmap
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class RegistrationViewModel : ViewModel() {
+class RegistrationViewModel @ViewModelInject constructor(
+    @Assisted private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
-    private var _profilePicture = MutableLiveData<Bitmap>()
+    companion object {
+        private const val PROFILE_PICTURE = "PROFILE_PICTURE"
+    }
+
+    private var _profilePicture: MutableLiveData<Bitmap>
     val profilePicture: LiveData<Bitmap>
         get() = _profilePicture
 
+    init {
+        _profilePicture = savedStateHandle.getLiveData(PROFILE_PICTURE)
+    }
+
     fun setProfilePicture(profilePicture: Bitmap) {
         _profilePicture.value = profilePicture
+        savedStateHandle.set(PROFILE_PICTURE, _profilePicture.value)
     }
 }
