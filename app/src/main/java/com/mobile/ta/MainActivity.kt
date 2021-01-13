ffff5a92a1727a67d4a40b2501ded1cae99dc820
Richard <richard.herew@gmail.com>
2021-01-14 00:14:01 +0700

    Added functionality to navigate on top-level destinations and created partial interface of home screen

--- app/src/main/java/com/mobile/ta/MainActivity.kt
+++ app/src/main/java/com/mobile/ta/MainActivity.kt
@@ -1,12 +1,74 @@
 package com.mobile.ta
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mobile.ta.R
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.mobile.ta.databinding.ActivityMainBinding
+import com.mobile.ta.ui.HomeFragmentDirections
+import com.mobile.ta.ui.MyCourseFragmentDirections
+import com.mobile.ta.ui.ProfileFragmentDirections
+import me.ibrahimsn.lib.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private var hideBottomBar = false
+    private val destinationChangedListener = NavController.OnDestinationChangedListener { _, destination, _ ->
+        hideBottomBar = when (destination.id) {
+            R.id.homeFragment -> false
+            R.id.myCourseFragment -> false
+            R.id.profileFragment -> false
+            else -> true
+        }
+        toggleBottomNavAnimation()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.act_main_host_fragment) as NavHostFragment).navController
+
+        // Delete from this line...
+        navController.navigate(HomeFragmentDirections.actionGlobalHomeFragment())
+        // ...until this line later
+        setContentView(binding.root)
+        setupBottomNavMenu(navController)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(destinationChangedListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(destinationChangedListener)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        binding.actMainBottomNav.onItemSelected = { pos ->
+            when (pos) {
+                0 -> navController.navigate(HomeFragmentDirections.actionGlobalHomeFragment())
+                1 -> navController.navigate(MyCourseFragmentDirections.actionGlobalMyCourseFragment())
+                2 -> navController.navigate(ProfileFragmentDirections.actionGlobalProfileFragment())
+            }
+        }
+    }
+
+    private fun toggleBottomNavAnimation() {
+        val translateDpValue =
+            if (hideBottomBar) resources.getDimension(R.dimen.bottom_nav_height) + 30f
+            else 0f
+        val translatePixelValue = translateDpValue * resources.displayMetrics.density
+
+        val animator =
+            ObjectAnimator.ofFloat(binding.actMainBottomNavContainer, View.TRANSLATION_Y, translatePixelValue)
+        animator.duration = 500
+        animator.start()
     }
 }
--- app/src/main/java/com/mobile/ta/ui/HomeFragment.kt
+++ app/src/main/java/com/mobile/ta/ui/HomeFragment.kt
@@ -1,6 +1,27 @@
 package com.mobile.ta.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mobile.ta.databinding.FragHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: FragHomeBinding? = null
+    private val binding get() = _binding as FragHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
