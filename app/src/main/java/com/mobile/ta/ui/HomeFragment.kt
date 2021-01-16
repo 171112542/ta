7561db113a20adf3344f4f8f5dc7053d4ec71ff2
Richard <richard.herew@gmail.com>
2021-01-17 01:16:43 +0700

    Added data, adapter, and navigation to search

--- app/src/main/java/com/mobile/ta/ui/HomeFragment.kt
+++ app/src/main/java/com/mobile/ta/ui/HomeFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mobile.ta.R
+import com.mobile.ta.adapter.CourseOverviewAdapter
+import com.mobile.ta.adapter.diff.CourseOverviewDiffCallback
+import com.mobile.ta.data.CourseOverviewData
 import com.mobile.ta.databinding.FragHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment :
+    Fragment(),
+    View.OnClickListener {
     private var _binding: FragHomeBinding? = null
     private val binding get() = _binding as FragHomeBinding
 
@@ -20,8 +26,26 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.frag_home_search_bar -> HomeFragmentDirections.actionHomeFragmentToSearchFragment()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val diffCallback = CourseOverviewDiffCallback()
+        val adapter = CourseOverviewAdapter(diffCallback)
+        adapter.submitList(CourseOverviewData.data.toMutableList())
+        binding.fragHomeRv.adapter = adapter
     }
 }
