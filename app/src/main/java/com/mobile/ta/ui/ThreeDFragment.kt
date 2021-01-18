d67b0b6b282f5c6e00fa550c8d6f32e8ba7d430f
Steven <stevenzlo122@gmail.com>
2021-01-19 04:28:15 +0700

    refactoring and fixing threedfragment

--- app/src/main/java/com/mobile/ta/ui/ThreeDFragment.kt
+++ app/src/main/java/com/mobile/ta/ui/ThreeDFragment.kt
@@ -1,28 +1,48 @@
 package com.mobile.ta.ui
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import com.mobile.ta.databinding.FragmentThreeDBinding
+import com.mobile.ta.viewmodel.course.ThreeDViewModel
+import java.util.*
+import kotlin.concurrent.schedule
 
 class ThreeDFragment : Fragment() {
     private lateinit var binding : FragmentThreeDBinding
+    private lateinit var mContext: Context
+    private val viewModel: ThreeDViewModel by viewModels()
+    private val args: ThreeDFragmentArgs by navArgs()
+    private var hasStartedTimer = false
+    private lateinit var timerTask: TimerTask
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentThreeDBinding.inflate(inflater, container, false)
+        mContext = requireContext()
         binding.apply {
-            webview.settings.allowContentAccess = true
-            webview.settings.javaScriptEnabled = true
-            webview.loadUrl("file:///android_asset/sketchfab-ex.html")
-            webview.webViewClient = object : WebViewClient() {
+            threeDWebView.settings.allowContentAccess = true
+            threeDWebView.settings.javaScriptEnabled = true
+            threeDWebView.loadUrl("file:///android_asset/sketchfab-ex.html")
+            threeDWebView.webViewClient = object : WebViewClient() {
                 override fun shouldOverrideUrlLoading(
                     view: WebView?,
                     request: WebResourceRequest?
@@ -30,7 +50,54 @@ class ThreeDFragment : Fragment() {
                     return true
                 }
             }
+            threeDWebView.addJavascriptInterface(SketchfabWebAppInterface(
+                args.sketchfabId,
+                viewModel,
+            ), "Android")
+            threeDBackInfoLabel.bringToFront()
+            threeDBackButton.setOnClickListener {
+                it.findNavController().navigateUp()
+            }
+
+            val timer = Timer()
+            viewModel.backButtonState.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+                threeDBackButton.visibility = if (it) View.VISIBLE else View.GONE
+                threeDBackInfoLabel.visibility = if (!it) View.VISIBLE else View.GONE
+                if (it) {
+                    cancelTimer(timer)
+                    startTimer(timer)
+                } else {
+                    cancelTimer(timer)
+                }
+            })
         }
         return binding.root
+    }
+
+    private fun startTimer(timer: Timer) {
+        timerTask = object : TimerTask() {
+            override fun run() {
+                hasStartedTimer = false
+                viewModel.setBackButtonState(false)
+            }
+        }
+        timer.schedule(timerTask, 5000)
+        hasStartedTimer = true
+    }
+    private fun cancelTimer(timer: Timer) {
+        if (hasStartedTimer) {
+            hasStartedTimer = false
+            timerTask.cancel()
+        }
+    }
+    class SketchfabWebAppInterface(private val sketchfabId: String, private val viewModel: ThreeDViewModel) {
+        @JavascriptInterface
+        fun getSketchfabId() : String {
+            return sketchfabId
+        }
+        @JavascriptInterface
+        fun toggleBackButton() {
+            viewModel.toggleBackButtonState()
+        }
     }
 }
