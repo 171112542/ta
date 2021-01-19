538f1a1839d8302c6fa972d469b94fb07ca9bb26
syntialai <53588149+syntialai@users.noreply.github.com>
2021-01-19 23:24:49 +0700

    Add settings interface and functionality
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ app/src/main/java/com/mobile/ta/ui/settings/SettingsFragment.kt
@@ -0,0 +1,40 @@
+package com.mobile.ta.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.mobile.ta.databinding.FragmentSettingsBinding
+import com.mobile.ta.ui.SettingsFragmentDirections
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        with(binding) {
+            textViewGiveAFeedback.setOnClickListener {
+                goToAddFeedback()
+            }
+            textViewLogOut.setOnClickListener {
+                goToLogIn()
+            }
+        }
+        return binding.root
+    }
+
+    private fun goToAddFeedback() {
+        findNavController().navigate(
+            SettingsFragmentDirections.actionSettingsFragmentToFeedbackFragment()
+        )
+    }
+
+    private fun goToLogIn() {
+        findNavController().navigate(SettingsFragmentDirections.actionGlobalLoginFragment())
+    }
+}
