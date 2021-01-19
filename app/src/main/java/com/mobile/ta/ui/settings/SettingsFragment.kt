b2fcd411af32967bb3ca3d99ac1778704d53354c
syntialai <53588149+syntialai@users.noreply.github.com>
2021-01-20 00:42:38 +0700

    Fix layout bugs
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- app/src/main/java/com/mobile/ta/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/mobile/ta/ui/settings/SettingsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mobile.ta.databinding.FragmentSettingsBinding
-import com.mobile.ta.ui.SettingsFragmentDirections
 
 class SettingsFragment : Fragment() {
 
