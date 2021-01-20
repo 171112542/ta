216bc01cb132e840e857150d94cd7150b82e87d6
syntialai <53588149+syntialai@users.noreply.github.com>
2021-01-21 01:18:56 +0700

    Add course information and course item interface
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- app/src/main/java/com/mobile/ta/ui/courseInfo/CourseInformationFragment.kt
+++ app/src/main/java/com/mobile/ta/ui/courseInfo/CourseInformationFragment.kt
@@ -1,6 +1,34 @@
 package com.mobile.ta.ui.courseInfo
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mobile.ta.databinding.FragmentCourseInformationBinding
 
 class CourseInformationFragment : Fragment() {
+
+    companion object {
+        private const val COURSE_INFORMATION_TAG = "COURSE_INFORMATION"
+    }
+
+    private lateinit var binding: FragmentCourseInformationBinding
+
+//    private val args: CourseInform
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCourseInformationBinding.inflate(inflater, container, false)
+        binding.buttonEnrollCourse.setOnClickListener {
+            openInputEnrollmentKeyBottomSheet()
+        }
+        return binding.root
+    }
+
+    private fun openInputEnrollmentKeyBottomSheet() {
+//        InputEnrollmentKeyBottomSheetDialogFragment.newInstance()
+    //        .show(parentFragmentManager, COURSE_INFORMATION_TAG)
+    }
 }
