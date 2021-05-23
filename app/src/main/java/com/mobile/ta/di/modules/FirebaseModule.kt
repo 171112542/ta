26d4c1cf24ed2a70e7181ed9a482c488643a12bd
Steven <stevenzlo122@gmail.com>
2021-05-23 21:01:28 +0700

    fix navigation bottom nav not sync, fix logout not forgetting the google account, fix home can navigate back to login

--- app/src/main/java/com/mobile/ta/di/modules/FirebaseModule.kt
+++ app/src/main/java/com/mobile/ta/di/modules/FirebaseModule.kt
@@ -1,13 +1,20 @@
 package com.mobile.ta.di.modules
 
+import android.R
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -24,4 +31,14 @@ class FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebaseStorageInstance(): FirebaseStorage = FirebaseStorage.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideGoogleSignInClientInstance(@ApplicationContext appContext: Context): GoogleSignInClient {
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        return GoogleSignIn.getClient(appContext, options)
+    }
 }
