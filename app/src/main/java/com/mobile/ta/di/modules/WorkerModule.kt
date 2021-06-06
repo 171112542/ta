c32967e31f9d8f41b32faa9fe80aa16ea8ae2179
171112542 <171112542@students.mikroskil.ac.id>
2021-06-06 23:32:43 +0700

    - Added a new Timer functionality by adding TestingTimerRepository.kt, TestingTimerRepositoryImpl.kt, TestingTimerViewModel.kt, and TestingTimerWorker.kt - Implemented the ViewModel in CourseContentFragment.kt and ThreeDFragment.kt - Added chapterId and courseId Navigation Parameters when navigating to ThreeDFragment.kt by modifying main_navigation.xml

---
+++ app/src/main/java/com/mobile/ta/di/modules/WorkerModule.kt
@@ -0,0 +1,20 @@
+package com.mobile.ta.di.modules
+
+import com.mobile.ta.repository.NotificationRepository
+import com.mobile.ta.repository.TestingTimerRepository
+import com.mobile.ta.repository.impl.NotificationRepositoryImpl
+import com.mobile.ta.repository.impl.TestingTimerRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+class WorkerModule {
+    @Provides
+    fun provideTimerRepository(timerRepositoryImpl: TestingTimerRepositoryImpl)
+            : TestingTimerRepository = timerRepositoryImpl
+}
---
+++ app/src/main/java/com/mobile/ta/repository/impl/TestingTimerRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.mobile.ta.repository.impl
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.mobile.ta.config.CollectionConstants
+import com.mobile.ta.model.testing.TimeSpent
+import com.mobile.ta.repository.TestingTimerRepository
+import com.mobile.ta.utils.fetchData
+import com.mobile.ta.utils.wrapper.status.Status
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.sql.Timestamp
+import javax.inject.Inject
+
+/**
+ * A Repository that contains the functions to save the timer
+ * of when the user starts navigating to a fragment and to save
+ * the timer of when the user finishes interacting with the fragment
+ */
+@ExperimentalCoroutinesApi
+class TestingTimerRepositoryImpl @Inject constructor(
+    val database: FirebaseFirestore
+): TestingTimerRepository {
+    private val userCollection by lazy {
+        database.collection(CollectionConstants.USER_COLLECTION)
+    }
+
+    override suspend fun saveTimeSpent(
+        userId: String,
+        courseId: String,
+        chapterId: String,
+        timeSpent: TimeSpent
+    ): Status<Boolean> =
+        userCollection.document(userId)
+            .collection(CollectionConstants.COURSE_COLLECTION)
+            .document(courseId)
+            .collection(CollectionConstants.CHAPTER_COLLECTION)
+            .document(chapterId)
+            .collection(CollectionConstants.TIME_COLLECTION)
+            .add(timeSpent)
+            .fetchData()
+}
