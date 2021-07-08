e1f28f99208370b313b19e01a156d262f032d2ce
171112542 <171112542@students.mikroskil.ac.id>
2021-07-08 16:01:19 +0700

    - Changed access of r/w from coursesDev collection back to courses collection

--- app/src/main/java/com/mobile/ta/config/CollectionConstants.kt
+++ app/src/main/java/com/mobile/ta/config/CollectionConstants.kt
@@ -1,7 +1,7 @@
 package com.mobile.ta.config
 
 object CollectionConstants {
-    const val COURSE_COLLECTION = "coursesDev"
+    const val COURSE_COLLECTION = "courses"
     const val CHAPTER_COLLECTION = "chapters"
     const val DISCUSSION_COLLECTION = "discussions"
     const val DISCUSSION_ANSWER_COLLECTION = "answers"
